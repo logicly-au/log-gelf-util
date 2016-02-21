f6b0c3e26cce98695bc5b8beb52a98ba224525fd
Adam Clarke <adam.clarke@strategicdata.com.au>
2016-02-21 11:16:41 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Log::GELF::Util - It's new $module
+
+# SYNOPSIS
+
+    use Log::GELF::Util;
+
+# DESCRIPTION
+
+Log::GELF::Util is ...
+
+# LICENSE
+
+Copyright (C) Adam Clarke.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Adam Clarke &lt;adam.clarke@strategicdata.com.au>
---
+++ lib/Log/GELF/Util.pm
@@ -0,0 +1,39 @@
+package Log::GELF::Util;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Log::GELF::Util - It's new $module
+
+=head1 SYNOPSIS
+
+    use Log::GELF::Util;
+
+=head1 DESCRIPTION
+
+Log::GELF::Util is ...
+
+=head1 LICENSE
+
+Copyright (C) Adam Clarke.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Adam Clarke E<lt>adam.clarke@strategicdata.com.auE<gt>
+
+=cut
+
